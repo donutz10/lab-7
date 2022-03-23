a71ff8b026f7e3cf6ffef1b5c2db98c87505bacd
Nawaf Al Naimi <nba2887@gmail.com>
2022-03-23 10:30:52 -0700

    added comments

--- lab7.py
+++ lab7.py
@@ -11,7 +11,7 @@ frame_width = int(video.get(3))
 frame_height = int(video.get(4))
 #saving size and importing frame and width
 size = (frame_width, frame_height)
-
+# to output the result
 result = cv2.VideoWriter('FaceDetect.avi', 
                          cv2.VideoWriter_fourcc(*'MJPG'),
                          12, size)
@@ -20,7 +20,7 @@ while True:
     frame, vid = video.read()
     # detecting the faces
     gray = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY) 
-
+    # read faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     #draw rectangle and text
     for (x, y, w, h) in faces:  
@@ -28,8 +28,9 @@ while True:
         cv2.putText(vid, 'Face', (x,y-10), Font, 0.5, (255,0,0), 2)
     # showing the video frame
     cv2.imshow('Video', vid)
+    #takes result of video and writes to FaceDetect.avi
     result.write(vid)
-
+    # 30 frames per second
     cv2.waitKey(30)
 
 result.release()
